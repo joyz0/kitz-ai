308da3dc2e9c5a5398ad30554a6b73378a0b56bc
justable <1214327383@qq.com>
2026-03-06 10:47:46 +0800

    test: remove index test file

--- src/config/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import * as configModule from '../index.js';
-
-describe('Config Index', () => {
-  it('should export expected modules', () => {
-    expect(configModule).toBeDefined();
-  });
-});
--- src/logger/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import * as loggerModule from '../index.js';
-
-describe('Logger Index', () => {
-  it('should export expected modules', () => {
-    expect(loggerModule).toBeDefined();
-  });
-});
